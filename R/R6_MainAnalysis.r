016cb308152ea5ce367dde48326f793c0b17d79b
KatoPachi <63113684+KatoPachi@users.noreply.github.com>
2023-08-17 19:22:32 +0900

    add balance_control method of MainAnalysis

--- R/R6_MainAnalysis.r
+++ R/R6_MainAnalysis.r
@@ -1,4 +1,5 @@
 library(R6)
+library(estimatr)
 
 MainAnalysis <- R6::R6Class("MainAnalysis",
   public = list(
@@ -34,6 +35,65 @@ MainAnalysis <- R6::R6Class("MainAnalysis",
       
       private$treat_labels <- treat_labels
       private$covs <- control
+    },
+    balance_control = function(
+      outcome_int = TRUE,
+      default_voucher = FALSE,
+      title = "",
+      notes = ""
+    ) {
+      val_coupon2019 <- ifelse(default_voucher, 1, 0)
+      dta <- if (outcome_int) self$wave1 else self$wave2
+      use <- subset(dta, coupon2019 == val_coupon2019)
+
+      use_covs <- use %>%
+        summarise_at(private$covs, list(~ sum(is.na(.)))) %>%
+        pivot_longer(everything()) %>%
+        dplyr::filter(value == 0) %>%
+        .$name
+
+      mu_table <- use %>%
+        group_by(nudge) %>%
+        summarize_at(use_covs, list(~ mean(.))) %>%
+        select(-coupon2019) %>%
+        mutate(nudge = factor(nudge, labels = private$treat_labels)) %>%
+        pivot_longer(-nudge) %>%
+        pivot_wider(names_from = nudge, values_from = value)
+
+      p <- mu_table$name %>%
+        sapply(function(x) {
+          mod <- reformulate("nudge", x)
+          est <- lm_robust(mod, data = use)
+          f <- est$fstatistic
+          pf(f[1], f[2], f[3], lower.tail = FALSE)
+        })
+
+      p_table <- tibble(
+        name = str_remove(names(p), ".value"),
+        "F-test, p-value" = p,
+      )
+
+      tbl <- mu_table %>%
+        left_join(p_table, by = "name")
+
+      tbl %>%
+        knitr::kable(
+          caption = title,
+          digits = 3,
+          col.names = c("", colnames(tbl)[-1]),
+          align = "lcccccccc"
+        ) %>%
+        kableExtra::kable_styling(
+          font_size = 9,
+          latex_options = "hold_position"
+        ) %>%
+        kableExtra::column_spec(2:8, width = "3em") %>%
+        kableExtra::footnote(
+          general_title = "",
+          general = notes,
+          threeparttable = TRUE,
+          escape = FALSE
+        )
     }
   ),
   private = list(
@@ -41,3 +101,6 @@ MainAnalysis <- R6::R6Class("MainAnalysis",
     treat_labels = c()
   )
 )
+
+a <- test$main_analysis()
+a$balance_control()

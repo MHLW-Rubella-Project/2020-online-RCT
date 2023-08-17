4dfb8e33d3d1c9e692f77de046c22b914d7e0df4
Hiroki Kato <hkato.econ@gmail.com>
2023-08-17 23:54:37 +0900

    fix class name and initialize method of MainAnalysis

--- R/R6_MainAnalysis.r
+++ R/R6_MainAnalysis.r
@@ -1,38 +1,13 @@
 library(R6)
 library(estimatr)
 
-MainAnalysis <- R6::R6Class("MainAnalysis",
+EstimateEffect <- R6::R6Class("EstimateEffect",
   public = list(
     wave1 = NULL,
     wave2 = NULL,
-    initialize = function(data, treat_labels, control) {
-      dt <- data %>%
-        mutate(
-          aw1_negative = if_else(act_vaccine != 4, 1, 0),
-          aw1_test_negative = aw1_test * aw1_negative,
-          coupon_a = coupon2019 * as.numeric(nudge == "A"),
-          coupon_b = coupon2019 * as.numeric(nudge == "B"),
-          coupon_c = coupon2019 * as.numeric(nudge == "C"),
-          coupon_d = coupon2019 * as.numeric(nudge == "D"),
-          coupon_e = coupon2019 * as.numeric(nudge == "E"),
-          coupon_f = coupon2019 * as.numeric(nudge == "F"),
-          coupon_g = coupon2019 * as.numeric(nudge == "G")
-        )
-      
-      self$wave1 <- dt %>%
-        filter(40 <= age & age <= 56) %>%
-        filter(exp_antibody != 1 & exp_vaccine != 1)
-      
-      self$wave2 <- dt %>%
-        filter(40 <= age & age <= 56) %>%
-        filter(follow == 1) %>%
-        filter(
-          exp_antibody != 1 &
-          exp_vaccine != 1 &
-          act_test != 2 &
-          act_vaccine != 2
-        )
-      
+    initialize = function(wave1, wave2, treat_labels, control) {
+      self$wave1 <- wave1
+      self$wave2 <- wave2
       private$treat_labels <- treat_labels
       private$covs <- control
     },
--- R/R6_SetData.r
+++ R/R6_SetData.r
@@ -73,7 +73,34 @@ SetData <- R6::R6Class("SetData",
       sprintf("F-value = %1.3f (p-value = %1.3f)", f, p)
     },
     main_analysis = function() {
-      MainAnalysis$new(self$data, private$treat_labels, private$covs)
+      dt <- self$data %>%
+        mutate(
+          aw1_negative = if_else(act_vaccine != 4, 1, 0),
+          aw1_test_negative = aw1_test * aw1_negative,
+          coupon_a = coupon2019 * as.numeric(nudge == "A"),
+          coupon_b = coupon2019 * as.numeric(nudge == "B"),
+          coupon_c = coupon2019 * as.numeric(nudge == "C"),
+          coupon_d = coupon2019 * as.numeric(nudge == "D"),
+          coupon_e = coupon2019 * as.numeric(nudge == "E"),
+          coupon_f = coupon2019 * as.numeric(nudge == "F"),
+          coupon_g = coupon2019 * as.numeric(nudge == "G")
+        )
+      
+      wave1 <- dt %>%
+        filter(40 <= age & age <= 56) %>%
+        filter(exp_antibody != 1 & exp_vaccine != 1)
+      
+      wave2 <- dt %>%
+        filter(40 <= age & age <= 56) %>%
+        filter(follow == 1) %>%
+        filter(
+          exp_antibody != 1 &
+          exp_vaccine != 1 &
+          act_test != 2 &
+          act_vaccine != 2
+        )
+      
+      EstimateEffect$new(wave1, wave2, private$treat_labels, private$covs)
     }
   ),
   private = list(
